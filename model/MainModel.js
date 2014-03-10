3e9f9c2ae68e361828f3d2e14eb1121a4df22318
Dale Corns <drcorns@hotmail.com>
2014-03-10 02:12:08 -0700

    Start refactor with backbone

---
+++ model/MainModel.js
@@ -0,0 +1,12 @@
+
+'use strict'
+//Model for Beginning of route Auto Complete type selection 1 = All, 2 = Geo, 3 = Establishment Default 1
+
+var FromType = Backbone.Model.extend({});
+var fromType = new FromType({
+	type:1, id:1
+});
+
+
+
+
---
+++ view/MainView.js
@@ -0,0 +1,14 @@
+
+var FromViewType = Backbone.View.extend({
+	template: _.template('<h3><%= type %></h3>'),
+	render: function(){
+		var attributes = this.model.toJSON();
+		this.$el.html(this.template(attributes));
+		return this;
+	}
+});
+var fromViewType = new FromViewType({model: fromType});
+
+
+var ViewCollection = Backbone.View.extend({});
+//var fromViewCollection = new ViewCollection({collection:fromModelCollection});
